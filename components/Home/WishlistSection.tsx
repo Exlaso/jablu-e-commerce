80a104703ca3227057c632a7ec9907f476e83f36
Exlaso <vedantbhavsar67@gmail.com>
2023-10-12 12:04:55 +0530

    fix issue

--- components/Home/WishlistSection.tsx
+++ components/Home/WishlistSection.tsx
@@ -6,18 +6,23 @@ import { cookies } from "next/headers";
 
 const WishlistSection = async () => {
   const token: RequestCookie | undefined = cookies().get("jablu_jwt_token");
-  const res = await fetch(
-    process.env.NEXTAUTH_URL +
-      `/api/Getallwishlist?jablu_jwt_token=${token?.value}`
-  );
-  let data = await res.json();
   let wishlistdata;
-  if (data.errorcode === "TCNF") {
-    wishlistdata = [];
-  } else {
-    wishlistdata = data.message.map((e: any) => ({ ...e.product }));
-  }
-
+  if (token?.value) {
+    
+    const res = await fetch(
+      process.env.NEXTAUTH_URL +
+      `/api/Getallwishlist?jablu_jwt_token=${token?.value}`
+      );
+      let data = await res.json();
+      if (data.errorcode === "TCNF") {
+        wishlistdata = [];
+      } else {
+        wishlistdata = data.message.map((e: any) => ({ ...e.product }));
+      }
+    }
+else{
+  wishlistdata = []
+}
   return (
     wishlistdata.length !== 0 && (
       <CardSection data={wishlistdata}>Wishlist</CardSection>
