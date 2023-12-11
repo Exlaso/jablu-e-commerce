7b97d719beb596fbdc6b3071c56b937fe8116312
Exlaso <vedantbhavsar67@gmail.com>
2023-12-11 16:38:09 +0530

    fixed timeout

---
+++ app/(home)/api/cronjob/SetFavouritedMail/route.ts
@@ -0,0 +1,51 @@
+import {NextRequest, NextResponse} from "next/server";
+import {Resend} from "resend";
+import {ProductReminderEmail} from "@/components/Emailsend";
+import {gqlClient} from "@/lib/service/client";
+import {GetFavouriteSubscribersDocument} from "@/lib/gql/graphql";
+export const dynamic = "force-dynamic"
+export const GET = async () => {
+
+    try {
+        const user = await gqlClient.request(GetFavouriteSubscribersDocument, {}, {
+            'x-hasura-admin-secret': process.env.Hasura_Secret as string
+        })
+
+        const resend = new Resend(process.env.RESEND_KEY as string);
+        for (const e of user.users) {
+            if (e.wishlist_items.length > 0) {
+                await new Promise(resolve => setTimeout(resolve, 110));
+
+                resend.emails.send({
+                    from: "Jablu.in <Jablu@exlaso.in>",
+                    to: [e.user_email],
+                    subject: `You have ${e.wishlist_items.length} products in your Favorites List`,
+                    html: ProductReminderEmail({
+                        name: e.user_first_name,
+                        products: e.wishlist_items,
+                        email: e.user_email
+                    })
+                }).catch((e) => {
+                    console.log(e)
+
+                });
+            }
+        }
+        return NextResponse.json({
+            message: "Success",
+            error: false,
+        })
+    } catch (error) {
+        console.error("Error in app/api/cronjob/route.ts: " + error)
+        if (error instanceof Error) {
+            return NextResponse.json({
+                message: error.message,
+                error: true,
+            })
+
+
+        }
+    }
+
+
+}
--- app/(home)/api/cronjob/route.ts
+++ app/(home)/api/cronjob/route.ts
@@ -1,9 +1,6 @@
 import {NextRequest, NextResponse} from "next/server";
-import {Resend} from "resend";
-import {ProductReminderEmail} from "@/components/Emailsend";
-import {gqlClient} from "@/lib/service/client";
-import {GetFavouriteSubscribersDocument} from "@/lib/gql/graphql";
 
+export const dynamic = "force-dynamic"
 export const GET = async (req: NextRequest) => {
     const authHeader = req.headers.get('authorization');
     if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
@@ -12,33 +9,17 @@ export const GET = async (req: NextRequest) => {
         });
     }
     try {
-        const user = await gqlClient.request(GetFavouriteSubscribersDocument, {}, {
-            'x-hasura-admin-secret': process.env.Hasura_Secret as string
-        })
 
-        const resend = new Resend(process.env.RESEND_KEY as string);
-        for (const e of user.users) {
-            if (e.wishlist_items.length > 0) {
-                await new Promise(resolve => setTimeout(resolve, 150));
 
-                await resend.emails.send({
-                    from: "Jablu.in <Jablu@exlaso.in>",
-                    to: [e.user_email],
-                    subject: `You have ${e.wishlist_items.length} products in your Favorites List`,
-                    html: ProductReminderEmail({
-                        name: e.user_first_name,
-                        products: e.wishlist_items,
-                        email: e.user_email
-                    })
-                });
-            }
+        const url = new URL(`${process.env.NEXTAUTH_URL}/api/cronjob/SetFavouritedMail`);
+        fetch(url)
+        const resdata = {
+            message: "success",
+            error: false
         }
-        return NextResponse.json({
-            message: "Success",
-            error: false,
-        })
+        return NextResponse.json(resdata)
     } catch (error) {
-        console.error("Error in app/api/cronjob/route.ts: " + error)
+        console.error("Error in app/(home)/api/cron2/route.ts: " + error)
         if (error instanceof Error) {
             return NextResponse.json({
                 message: error.message,
